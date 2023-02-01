d111bf159fe6aac60c2e3fc6f85f98cf5658158e
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-02-01 19:03:22 +0300

    Add files via upload

---
+++ Implementation/src/torchlite/DType.cs
@@ -0,0 +1,47 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2023. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+namespace System.AI.Experimental
+{
+
+    public static partial class torchlite
+    {
+
+        /// <summary>
+        /// Represents the torchlite supported data type for the tensor.
+        /// </summary>
+        public enum DType : byte
+        {
+
+            /// <summary>
+            /// 32-bit floating point. Alias for float.
+            /// </summary>
+            float32 = 0,
+
+            /// <summary>
+            /// 32-bit floating point. Alias for float32.
+            /// </summary>
+            @float = 0,
+
+            /// <summary>
+            /// 32-bit signed integer. Alias for int.
+            /// </summary>
+            int32 = 1,
+
+            /// <summary>
+            /// 32-bit signed integer. Alias for int32.
+            /// </summary>
+            @int = 1,
+
+            /// <summary>
+            /// Boolean.
+            /// </summary>
+            @bool = 2
+
+        }
+
+    }
+
+}
