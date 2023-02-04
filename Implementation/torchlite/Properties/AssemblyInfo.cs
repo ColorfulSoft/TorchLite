91f4cdf2cad39d749cab1f829d32b12908ade8a1
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-02-04 22:50:13 +0300

    Add files via upload

--- Implementation/torchlite/Properties/AssemblyInfo.cs
+++ Implementation/torchlite/Properties/AssemblyInfo.cs
@@ -1,1 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("torchlite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("torchlite")]
+[assembly: AssemblyCopyright("(C) ColorfulSoft corp., 2019-2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
