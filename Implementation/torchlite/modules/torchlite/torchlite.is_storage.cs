6c07370f61ce4ca955860b71d3aad23926bd2b5e
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-02-07 10:51:03 +0300

    Add files via upload

---
+++ Implementation/torchlite/modules/torchlite/torchlite.is_storage.cs
@@ -0,0 +1,24 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2023. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+namespace System.AI.Experimental
+{
+
+    public static partial class torchlite
+    {
+
+        /// <summary>
+        /// Returns true if obj is a TorchLite storage.
+        /// </summary>
+        /// <param name="obj">Object to test.</param>
+        /// <returns>obj is Storage</returns>
+        public static bool is_storage(this object obj)
+        {
+            return obj is Storage;
+        }
+
+    }
+
+}
