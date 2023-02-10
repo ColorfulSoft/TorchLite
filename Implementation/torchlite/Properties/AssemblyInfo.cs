4859a429a5373425663c5d2907289cd2f57f7e4f
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-02-10 10:34:48 +0300

    Add files via upload

--- Implementation/torchlite/Properties/AssemblyInfo.cs
+++ Implementation/torchlite/Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("torchlite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An open source machine learning framework that accelerates the path from research prototyping to production deployment.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("torchlite")]
+[assembly: AssemblyCompany("ColorfulSoft")]
+[assembly: AssemblyProduct("TorchLite")]
 [assembly: AssemblyCopyright("(C) ColorfulSoft corp., 2019-2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("The System.AI Project")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.1.0.0")]
