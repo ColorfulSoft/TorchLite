732546069762fe1b3cf50e3e74f1a3340b3816c1
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-02-10 11:25:43 +0300

    Add files via upload

---
+++ Implementation/torchlite/modules/torchlite/torchlite.load.cs
@@ -0,0 +1,88 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2023. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+using System.IO;
+using System.Collections.Generic;
+
+namespace System.AI.Experimental
+{
+
+    public unsafe static partial class torchlite
+    {
+
+        /// <summary>
+        /// Loads an object saved with torch.save() from a file.
+        /// </summary>
+        /// <param name="f">A readable stream object or a string or Uri object containing a file name.</param>
+        /// <returns>Dictionary&lr;string, Tensor&gt;.</returns>
+        public static Dictionary<string, Tensor> load(Stream f)
+        {
+            var br = new BinaryReader(f);
+            var state_dict = new Dictionary<string, Tensor>();
+            try
+            {
+                if(br.ReadString() != "TorchLite")
+                {
+                    throw new Exception();
+                }
+                while((br.BaseStream.Position + 1) < br.BaseStream.Length)
+                {
+                    var name = br.ReadString();
+                    var dtype = (DType)br.ReadByte();
+                    var shape = new int[br.ReadInt32()];
+                    for(int i = 0; i < shape.Length; ++i)
+                    {
+                        shape[i] = br.ReadInt32();
+                    }
+                    var tensor = new Tensor(shape, dtype);
+                    var nbytes = tensor.shape.numel() * dtype.size();
+                    var bytes = (byte*)tensor.storage.data_ptr;
+                    for(int i = 0; i < nbytes; ++i)
+                    {
+                        bytes[i] = br.ReadByte();
+                    }
+                    state_dict.Add(name, tensor);
+                }
+            }
+            catch(Exception e)
+            {
+                throw new IOException("f is not valid TorchLite file.");
+            }
+            return state_dict;
+        }
+
+        /// <summary>
+        /// Loads an object saved with torch.save() from a file.
+        /// </summary>
+        /// <param name="f">A readable stream object or a string or Uri object containing a file name.</param>
+        /// <returns>Dictionary&lr;string, Tensor&gt;.</returns>
+        public static Dictionary<string, Tensor> load(Uri f)
+        {
+            Dictionary<string, Tensor> state_dict;
+            using(var s = File.OpenRead(f.AbsolutePath))
+            {
+                state_dict = torchlite.load(s);
+            }
+            return state_dict;
+        }
+
+        /// <summary>
+        /// Loads an object saved with torch.save() from a file.
+        /// </summary>
+        /// <param name="f">A readable stream object or a string or Uri object containing a file name.</param>
+        /// <returns>Dictionary&lr;string, Tensor&gt;.</returns>
+        public static Dictionary<string, Tensor> load(string f)
+        {
+            Dictionary<string, Tensor> state_dict;
+            using(var s = File.OpenRead(f))
+            {
+                state_dict = torchlite.load(s);
+            }
+            return state_dict;
+        }
+
+    }
+
+}
---
+++ Implementation/torchlite/modules/torchlite/torchlite.save.cs
@@ -0,0 +1,73 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2023. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+using System.IO;
+using System.Collections.Generic;
+
+namespace System.AI.Experimental
+{
+
+    public unsafe static partial class torchlite
+    {
+
+        /// <summary>
+        /// Saves an object to a disk file.
+        /// </summary>
+        /// <param name="obj">Saved object.</param>
+        /// <param name="f">A writable stream object or a string or Uri object containing a file name.</param>
+        public static void save(Dictionary<string, Tensor> obj, Stream f)
+        {
+            var bw = new BinaryWriter(f);
+            bw.Write("TorchLite");
+            foreach(var parameter in obj)
+            {
+                bw.Write(parameter.Key);
+                var tensor = parameter.Value;
+                bw.Write((byte)tensor.dtype);
+                var ndim = tensor.shape.ndim;
+                bw.Write(ndim);
+                var shape = tensor.shape.data_ptr;
+                for(int i = 0; i < ndim; ++i)
+                {
+                    bw.Write(shape[i]);
+                }
+                var nbytes = tensor.shape.numel() * tensor.dtype.size();
+                var bytes = (byte*)tensor.storage.data_ptr;
+                for(int i = 0; i < nbytes; ++i)
+                {
+                    bw.Write(bytes[i]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Saves an object to a disk file.
+        /// </summary>
+        /// <param name="obj">Saved object.</param>
+        /// <param name="f">A writable stream object or a string or Uri object containing a file name.</param>
+        public static void save(Dictionary<string, Tensor> obj, Uri f)
+        {
+            using(var s = File.Create(f.AbsolutePath))
+            {
+                torchlite.save(obj, s);
+            }
+        }
+
+        /// <summary>
+        /// Saves an object to a disk file.
+        /// </summary>
+        /// <param name="obj">Saved object.</param>
+        /// <param name="f">A writable stream object or a string or Uri object containing a file name.</param>
+        public static void save(Dictionary<string, Tensor> obj, string f)
+        {
+            using(var s = File.Create(f))
+            {
+                torchlite.save(obj, s);
+            }
+        }
+
+    }
+
+}
